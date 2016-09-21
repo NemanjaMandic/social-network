ae3e7be7319fc09525afd9800b6a1c12ad36b36d
nemanjamandic <nemanjans84@gmail.com>
2016-09-21 16:19:45 +0000

    added bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ActiveRecord::Base
     has_many :posts, dependent: :destroy
     has_many :text_posts, dependent: :destroy
     has_many :image_posts, dependent: :destroy
+    has_many :comments, dependent: :destroy
     
     def following?(leader)
         leaders.include? leader
