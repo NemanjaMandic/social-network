7e155196292733a08ad460573160c34071e237a4
nemanjamandic <nemanjans84@gmail.com>
2016-09-22 17:05:56 +0000

    follow button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,19 @@ class UsersController < ApplicationController
         end
     end
     
+    def show
+        @user = User.find(params[:id])
+        @posts = @user.posts.order("created_at DESC")
+    end
+    
+    def follow
+       @user = User.find(params[:id])
+         if current_user.follow!(@user)
+            redirect_to @user, notice: "Follow successful!"
+         else
+            redirect_to @user, alert: "Error following."
+        end
+    end
     private
     def user_params
         params.require(:user).permit(:name, :email, :password, :password_confirmation)
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
    get 'login', to: 'sessions#new', as: 'login'
    get 'logout', to: 'sessions#destroy', as: 'logout'
    
+   get 'follow/:id', to: 'users#follow', as: 'follow_user'
+   
    root 'posts#index'
    
 
