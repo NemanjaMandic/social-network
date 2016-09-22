48ba69cff827a35ca1762851c39130210c31085e
nemanjamandic <nemanjans84@gmail.com>
2016-09-22 00:14:43 +0000

    login user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,19 @@
 class SessionsController < ApplicationController
+    
+    def new
+    end
+    def create
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to root_url, notice: "Log in successfull!"
+        else
+            flash.now.alert = "Invalid email password"
+        end
+    end
+    
+    def destroy
+        session[:user_id] = nil
+        redirect_to root_url, notice: "Successfully log out"
+    end
 end
