9cdf8e43f9d5ae3f17defdc11e5818fc9014de5a
nemanjamandic <nemanjans84@gmail.com>
2016-09-24 21:34:55 +0000

    before action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    before_action :authenticate_user!
     
     def new
         @user = User.new
@@ -26,6 +27,7 @@ class UsersController < ApplicationController
             redirect_to @user, alert: "Error following."
         end
     end
+    
     private
     def user_params
         params.require(:user).permit(:name, :email, :password, :password_confirmation)
