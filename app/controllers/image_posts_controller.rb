f321022be766f520962c03de292a2d31a85c745f
nemanjamandic <nemanjans84@gmail.com>
2016-09-24 23:23:04 +0000

    added edit

--- app/controllers/image_posts_controller.rb
+++ app/controllers/image_posts_controller.rb
@@ -1,2 +1,21 @@
 class ImagePostsController < ApplicationController
+    
+    def new 
+        @image_post = ImagePost.new
+    end
+    
+    def create 
+        @image_post = ImagePost.new(image_post_params)
+        if @image_post.save
+            redirect_to @image_post 
+        else
+            render 'new'
+        end
+    end
+    
+    private
+    
+    def image_post_params
+        params.require(:image_post).permit(:url)
+    end
 end
